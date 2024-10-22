1f7f7f177cd49048cee255ed4f4b752de9481f82
Chis Narcisa-Maria <chisnarcisa45@gmail.com>
2024-10-22 14:46:36 +0000

    Tema laborator 2

--- tema 2.py
+++ tema 2.py
@@ -1,6 +1,4 @@
 import string
-
-
 my_articol = """
 Python este instrumentul principal pentru dezvoltarea proiectelor AI și machine learning (ML); prin urmare, este cea mai importantă tehnologie pe care programatorii AI o folosesc în munca lor.  Python este un limbaj pe care îl veți învăța cu ușurință și cu ajutorul căruia, cu puțin efort, veți putea obține rezultate foarte bune când vine vorba de programare AI și ML.
 """
